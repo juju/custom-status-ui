c926f3b126061ad2a1e2b60e68cb7e846514c97e
Jeff Pihach <jeff.pihach@canonical.com>
2018-07-06 11:46:28 -0600

    Move server.js to server folder and add interactions with the juju module.

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const WebSocketServer = require('websocket').server;
+
+const {connectAndWatch, cancelWatch} = require('./juju');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(404);
+  res.end();
+});
+server.listen(8080, () => console.log('Server is listening on port 8080'));
+
+wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+let watchHandle = null;
+
+const connectionOptions = {
+  controllerIP: process.env.CONTROLLERIP,
+  modelUUID: process.env.MODELUUID,
+  userName: process.env.USERNAME,
+  password: process.env.PASSWORD
+};
+
+wsServer.on('request', request => {
+  const conn = request.accept('', request.origin);
+  conn.on('message', msg => {
+    if (msg.utf8Data === 'start') {
+      connectAndWatch(connectionOptions, msg => conn.sendUTF(JSON.stringify(msg)),
+        (err, handle) => {
+          if (err) {
+            conn.sendUTF({error: err});
+            if (handle) {
+              cancelWatch(handle);
+            }
+            return;
+          }
+          watchHandle = handle;
+        });
+    }
+  });
+  conn.on('close', (reasonCode, description) => {
+    if (watchHandle) {
+      cancelWatch(watchHandle);
+    }
+  });
+});
